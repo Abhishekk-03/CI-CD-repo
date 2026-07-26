b569e892342d00205ec59dcd030dd3914d893ed8
Abhishek Kumar <avhikumar52@gmail.com>
2026-07-26 09:51:54 +0530

    imlement test

---
+++ Application/Test_Calculator.py
@@ -0,0 +1,23 @@
+import pytest
+from Application.Calculator import add, subtract, multiply, divide
+
+
+def test_add():
+    assert add(5, 5) == 10
+
+
+def test_subtract():
+    assert subtract(10, 5) == 5
+
+
+def test_multiply():
+    assert multiply(5, 5) == 25
+
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10, 0)
